19afa8baf5ce514a554217c77a992026c5cc1e2b
Bashaer <110439978+bashaer310@users.noreply.github.com>
2023-08-11 08:11:35 +0300

    Update EmployeeModel.java

--- Assignment11/src/main/java/com/example/assignment11/Model/EmployeeModel.java
+++ Assignment11/src/main/java/com/example/assignment11/Model/EmployeeModel.java
@@ -7,34 +7,34 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class EmployeeModel {
-
-    @NotEmpty(message = "It shouldn't be empty")
-    @Min(2)
+    
+    @NotEmpty(message = "Name shouldn't be empty")
+    @Size(min = 2)
     private String ID;
 
-    @NotEmpty(message = "It shouldn't be empty")
-    @Min(4)
+    @NotEmpty(message = "Name shouldn't be empty")
+    @Size(min = 2)
     private String name;
 
-    @NotNull(message = "It shouldn't be empty")
-    //@Pattern(regexp = "[0-9]+")
-    //@Digits(integer = 2,fraction = 0)
-    @Min(25)
-    private int age;
 
-    @NotEmpty(message = "It shouldn't be empty")
-    //@Pattern(regexp = "supervisor|coordinator")
+    @NotEmpty(message = "Age shouldn't be empty")
+    @Pattern(regexp="\\d+", message="Age should be Integer")
+    @Min(25)
+    private String age;
+
+    @NotEmpty(message = "Position shouldn't be empty")
+    @Pattern(regexp = "supervisor|coordinator", message = "Position should be supervisor or coordinator  ")
     private String position;
 
-    @AssertFalse(message = "It shouldn't be true ")
-    private boolean onLeave;
+    @AssertFalse(message = "On Leave shouldn't be true ")
+    private Boolean onLeave;
 
-    @NotNull(message = "It shouldn't be empty")
-    //@Pattern(regexp = "[0-9]+")
-    //@PastOrPresent
-    private int employmentYear;
+    @NotEmpty(message = "Employment Year shouldn't be empty")
+    @Pattern(regexp="\\d{4}", message="Employment Year should be a valid format")
+    //@Max(LocalDate.now().getYear())
+    private String employmentYear;
 
-    @NotNull(message = "It shouldn't be empty")
-    //@Pattern(regexp = "[0-9]+")
-    private int annualLeave;
+    @NotEmpty(message = "Annual Leave shouldn't be empty")
+    @Pattern(regexp="\\d+", message="Annual Leave should be Integer")
+    private String annualLeave;
 }
